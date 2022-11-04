1f5e5401b1db0b2aa73f85d12f10a24215528837
Joakim Jarting <96111418+Brulize@users.noreply.github.com>
2022-11-04 11:35:29 +0100

    Nu ni!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-startsidan
 import Login from "./components/Login.js";
 import Register from "./components/Register.js";
 
 
 function App() {
   return (
-    <>
+    
     <div>
+      <h1>Escape The Woods</h1>
       <Login />
       <Register />
-      
+    </div>
+  );
+}
+
 export default App;
