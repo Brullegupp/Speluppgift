f8fba0eb0419869977cb9c39df1cf21c4ec0183d
Autismotus <isakthorstromryahoo.se>
2022-11-04 11:21:50 +0100

    testar igen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-startsidan
+
 import Login from "./components/Login.js";
 import Register from "./components/Register.js";
 
 
 function App() {
   return (
-    <>
+    
     <div>
       <Login />
       <Register />
+      </div>
+      )}
       
 export default App;
