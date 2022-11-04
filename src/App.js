a0f84cc72e1fa3d0ec53c4d905500deb88ccffec
Autismotus <isakthorstromryahoo.se>
2022-11-04 13:31:33 +0100

    spelmeny

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-startsidan
+import Menu from "./components/Menu.js";
 import Login from "./components/Login.js";
 import Register from "./components/Register.js";
 
 
 function App() {
   return (
-    <>
+
     <div>
       <Login />
       <Register />
-      
+      <Menu />
+      </div>
+      )}
 export default App;
