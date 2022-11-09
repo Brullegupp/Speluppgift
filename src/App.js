150ba5a28b7b6df41deb252ff3910957e126b370
Autismotus <isakthorstromryahoo.se>
2022-11-09 20:26:58 +0100

    login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import Menu from "./components/Menu.js";
 import Login from "./components/Login.js";
 import Register from "./components/Register.js";
-
+import './index.css'
 
 function App() {
   return (
 
     <div>
       <Login />
-      <Register />
-      <Menu />
       </div>
       )}
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,27 @@
-
 const Login = () => {
     return (
-        <div>
+        <div className="divlogin">
             <form className="form">
-                <h2>Login</h2>
-                    <li>
-                    <label className="form-control">
-                        Username:
-                        <input type="text" id="username"></input>
-                     
-                    </label>
-                    </li>
-                    <li>
-                    <label className="form-control">
-                        Password:
-                        <input type="password" id="password"></input>
-                    </label>
-                    </li>
-                    <input type="submit" value="Login"></input>
-            
+                <h2 className="loginh2">Login</h2>
+
+                <label className="form-control">
+                    Username:
+                    <input type="text" id="username"></input>
+
+                </label>
+
+
+                <label className="form-control">
+                    Password:
+                    <input type="password" id="password"></input>
+                </label>
+
+                <input type="submit" value="Login"></input>
+
             </form >
             <button className="register-btn1" onClick="click">Register</button>
         </div>
-    )
+    );
 
 }
 
