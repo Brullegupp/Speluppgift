0934f424adbd5e9817e2da553731faf390db47c0
Fabian Shahshahani <fabiansha@gmail.com>
2022-11-10 15:05:53 +0100

    routing

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,34 +1,37 @@
-
+import { useNavigate } from "react-router-dom"
 
 const Register = () => {
+
+    const navigate = useNavigate()
+
     return (
         <div>
             <form className="form">
                 <h2>Register</h2>
-                <li>
+                
                     <label className="form-control">
                         Email:
                         <input type="email" id="email"></input>
 
-                    </label>
-                </li>
-                <li>
+                    </label><br></br>
+                
+                
                     <label className="form-control">
                         Username:
                         <input type="text" id="username"></input>
 
-                    </label>
-                </li>
-                <li>
+                    </label><br></br>
+                
+                
                     <label className="form-control">
                         Password:
                         <input type="password" id="password"></input>
                     </label>
-                </li>
+                
                 
 
             </form >
-            <button className="register-btn2" onClick="click">Register</button></div>
+            <button className="register-btn2" onClick={() => {navigate("/")}}>Register</button></div>
     )
 }
 
