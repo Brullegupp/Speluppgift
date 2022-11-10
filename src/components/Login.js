039d9be6c0eea748f812190fa38848f08ae2ffee
Autismotus <isakthorstromryahoo.se>
2022-11-10 10:04:56 +0100

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,28 @@
+
 const Login = () => {
     return (
-        <div className="divlogin">
-            <form className="form">
-                <h2 className="loginh2">Login</h2>
+        <>
+            <div className="divlogin">
+                <h1 className="titel">Escape the woods</h1>
+                <form className="form">
+                    <h2 className="loginh2">Login</h2>
+                    <label className="form-control">
+                        Username:
+                        <input type="text" id="username"></input>
+                    </label>
+                    <label className="form-control">
+                        Password:
+                        <input type="password" id="password"></input>
+                    </label>
+                    <input type="submit" value="Login"></input>
+                </form >
+                <button className="register-btn1" onClick="click">Register</button>
+                <button className="exit-btn1" onClick="click">Exit</button>
+            </div>
 
-                <label className="form-control">
-                    Username:
-                    <input type="text" id="username"></input>
-
-                </label>
-
-
-                <label className="form-control">
-                    Password:
-                    <input type="password" id="password"></input>
-                </label>
-
-                <input type="submit" value="Login"></input>
-
-            </form >
-            <button className="register-btn1" onClick="click">Register</button>
-        </div>
+        </>
     );
+
 
 }
 
