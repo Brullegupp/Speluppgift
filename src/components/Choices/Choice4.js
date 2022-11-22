e9306087b20c44e687fd90398a9170586c467105
Fabian Shahshahani <fabiansha@gmail.com>
2022-11-22 17:19:52 +0100

    choice4 fixad

--- src/components/Choices/Choice4.js
+++ src/components/Choices/Choice4.js
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom"
+import Text from "../Choices/Text.js"
 
 const Choice4 = () => {
 
@@ -6,8 +7,9 @@ const Choice4 = () => {
     return (
         <div className="choice4">
         <div className="choice-btn">
-            <button className="choice-btn" onClick={() => { navigate("/goal") }}>Arrow</button>
-            <button className="choice-btn" onClick={() => { navigate("/Death1") }}>Arrow</button>
+            <Text />
+            <button className="choice-btn7" onClick={() => { navigate("/goal") }}>This way</button>
+            <button className="choice-btn8" onClick={() => { navigate("/Death1") }}>This way</button>
             <button className="exit-btn1" onClick={() => { navigate("/menu") }}>Exit</button>
             </div>
             </div>
