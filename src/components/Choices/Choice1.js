e65e9caef83c29dbea1502985248699c8573641c
Fabian Shahshahani <fabiansha@gmail.com>
2022-11-22 16:03:38 +0100

    css+text card

--- src/components/Choices/Choice1.js
+++ src/components/Choices/Choice1.js
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom"
+import Text from "./components/Choices/Text";
 
 const Choice1 = () => {
 
---
+++ src/components/Choices/Text.js
@@ -0,0 +1,14 @@
+
+const Text = () => {
+    return(
+        <div className="container">
+            <div className="text-card">
+                <h1>Select your way</h1>
+                <p>Choose one of the buttons below to pass through the woods.</p><br></br>
+                <p>If you choose wrong, you might not survive</p>
+            </div>
+        </div>
+    )
+}
+
+export default Text
