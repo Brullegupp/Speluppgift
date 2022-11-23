1e60f6addfe16588e4192238ca540b4502d986db
Fabian Shahshahani <fabiansha@gmail.com>
2022-11-23 14:42:20 +0100

    goal

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,7 +9,7 @@ const Menu = () => {
         <div className="menu-card">
             <h1 className="titel-name">Escape the woods</h1>
             <button className="play-btn" onClick={() => { navigate("/Story") }}>Play</button>
-            <button className="logout-btn" onClick={() => { navigate("/Login") }}>Log out</button>
+            <button className="logout-btn" onClick={() => { navigate("/") }}>Log out</button>
             <button className="exit-btn1" onClick="self.close()">Exit</button>
             
         </div>
--- src/components/goal.js
+++ src/components/goal.js
@@ -6,7 +6,7 @@ const Goal = () => {
     return (
         <div className="Goal-background">
         <div className="Goal">
-        <h1>You made it!</h1>
+        <h1 className="congrats">Congratulations!<br></br>You have now escaped.</h1>
         <button className="playagain-btn" onClick={() => { navigate("/choice1") }}>Play again</button><br></br>
         <button className="exit-btn" onClick={() => { navigate("/Menu") }}>Exit</button><br></br>
         </div>
