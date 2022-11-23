5b3128a86f5926eabc2b1d5c8fde7525091323de
Fabian Shahshahani <fabiansha@gmail.com>
2022-11-23 13:44:10 +0100

    textcard

--- src/components/Choices/Text.js
+++ src/components/Choices/Text.js
@@ -4,8 +4,11 @@ const Text = () => {
         <div className="container">
             <div className="text-card">
                 <h1>Which way?</h1><br></br>
-                <p>Select one of the two buttons</p><br></br>
-                <p>If you choose wrong, you might not survive</p>
+
+
+                <p className="text-card-text">Select one of the two buttons below</p><br></br>
+
+                <p className="text-card-text">If you choose wrong, you might not survive...</p>
                 
             </div>
         </div>
