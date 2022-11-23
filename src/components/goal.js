4fb152b2b52ff7982a46a515ba824d7fb9b4d39a
Fabian Shahshahani <fabiansha@gmail.com>
2022-11-23 15:12:14 +0100

    lite update

--- src/components/goal.js
+++ src/components/goal.js
@@ -4,8 +4,8 @@ const Goal = () => {
 
     const navigate = useNavigate()
     return (
-        <div className="Goal-background">
-        <div className="Goal">
+        <div className="goal-background">
+        <div className="goal">
         <h1 className="congrats">Congratulations!<br></br>You have now escaped.</h1>
         <button className="playagain-btn" onClick={() => { navigate("/choice1") }}>Play again</button><br></br>
         <button className="exit-btn" onClick={() => { navigate("/Menu") }}>Exit</button><br></br>
