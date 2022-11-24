ef4d86fe2de3051cd6f83b6faedca086d885d67c
Brulize <brulize@hotmail.com>
2022-11-24 08:10:30 +0100

    update

--- src/components/Deaths/Death3.js
+++ src/components/Deaths/Death3.js
@@ -5,13 +5,13 @@ const Death3 = () => {
     const navigate = useNavigate()
     return (
         <div className="overlay-death3">
-        <div className="menu-card-death">
-        <div className="death">
-        <h1 className="game-over">Game over</h1>
-                <button className="playagain-death" onClick={() => { navigate("/choice1") }}>Play again</button><br></br>
-                <button className="exit-death" onClick={() => { navigate("/Menu") }}>Exit</button><br></br>
-        </div>
-        </div>
+            <div className="menu-card-death">
+                <div className="death">
+                    <h1 className="game-over">Game over</h1>
+                    <button className="playagain-death" onClick={() => { navigate("/choice1") }}>Play again</button><br></br>
+                    <button className="exit-death" onClick={() => { navigate("/Menu") }}>Exit</button><br></br>
+                </div>
+            </div>
         </div>
     )
 }
